33a851d2acc3f7c1bca9e57a7f0c1590cb20e022
ImreEdem <imre.adam.686@dszcbaross.edu.hu>
2024-02-07 10:03:57 +0100

    elveszettdizályn0207

--- Proba2.js
+++ Proba2.js
@@ -32,15 +32,24 @@ const App = () => {
           keyExtractor={({ elveszett_id }) => elveszett_id.toString()}
           renderItem={({ item }) => (
             <View>
+              <View style={{padding:5,borderLeftColor:'blue',borderLeftWidth:7,borderRightColor:'blue',borderRightWidth:7,margin:1,borderTopLeftRadius:10,borderBottomLeftRadius:10,borderTopRightRadius:10,borderBottomRightRadius:10}}>
+              <View style={{borderBottomColor:'blue',borderBottomWidth:10,borderBottomStartRadius:12,borderBottomEndRadius:12,borderTopColor:'blue',borderTopWidth:7,borderTopStartRadius:12,borderTopEndRadius:12, marginBottom:5}} >
               {item.elveszett_kep && (
                 <Image
                 style={{ width: 300, height: 300, margin:5 }}
                 source={{ uri: `http://nodejs2.dszcbaross.edu.hu:22004/kepek/${item.elveszett_kep}` }}
               />
               )}
-              <Text style={{marginLeft:5, marginBottom:5}}>{item.elveszett_szoveg}</Text>
-              <View style={{borderWidth:1}}/>
+              <Text style={{marginLeft:5, marginBottom:5, textAlign:'center', padding:10, fontSize:20}}>{item.elveszett_szoveg}</Text>
+              </View>
+              
+              
+              
+              
+              
             </View>
+          </View>
+            
           )}
         />
       )}
